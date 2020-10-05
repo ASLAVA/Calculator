1696e305db8f6cdaf7e1a94ce703943ac71dabfa
Slava <vsa18@scaretmail.rutgers.edu>
2020-10-05 14:07:27 -0400

    added functionality to numbers/actions. next step is to add the math

---
+++ calculations.js
@@ -0,0 +1,17 @@
+function add() {
+
+}
+
+function subtract(){
+
+}
+function multiplication(){
+
+}
+function division(){
+
+}
+
+export {add, multiplication, subtract, division}
+
+
--- script.js
+++ script.js
@@ -1,46 +1,66 @@
-const zero = document.getElementById("");
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
+import {add, subtract, multiplication, division} from "./calculations.js";
+
 const clear = document.getElementById("clear");
-const backspace = document.getElementById("backbutton");
 const div = document.getElementById("div");
 const mult = document.getElementById("mult");
 const sub = document.getElementById("subt");
 const add = document.getElementById("add");
 const equal = document.getElementById("equal");
 const display = document.getElementById("display");
-
+const decimal = document.getElementById("dec");
 
 const numbers = document.getElementsByClassName('number');
 const action = document.getElementsByClassName('action');
 const operative = document.getElementsByClassName('operative');
-var currentArray = [];
-var totalArray = [];
 
-function singleNum(arg) {
-    let returnVal = "";
-    for(let i=0; i < arg.length; i++){
-        returnVal += arg[i];
+let decFlag = 0;
+
+//Listner for numbers. Calls numInput to add numbers to display text string.
+//goal is to take that string when operand is called and convert to int/float
+//and then put it in array or stack for later calculations.
+for(let i = 0; i<numbers.length; ++i){
+    numbers.item(i).addEventListener('click', numInput);
+}
+function numInput(){
+    let input = this.textContent;
+    if(input === '.'){
+        this.removeEventListener('click', numInput);
+        decFlag++;
     }
-    return parseFloat(returnVal);
+    if (display.textContent === '0')
+        if(input === '.'){
+            display.textContent += input;
+        }
+        else display.textContent = input;
+    else {
+        display.textContent += input;
+        
+              
+    } 
 }
 
-function operandCall(input) {
-    if (!currentArray[currentArray.length-1].match(/\d/g))
-        break;
-    if (currentArray[currentArray.length-1].match(/./g)){
-        currentArray.push(0);
+//listeners for clear and backspace buttons
+for(let i = 0; i < action.length; ++i){
+    action.item(i).addEventListener('click', clean);
+}
+//resets display to 0 on clear and if last number was backspaced
+//
+function clean(){
+    if (this.matches('#clear') || display.textContent.length == 1) {
+        display.textContent = 0;
+        if (decFlag > 0){
+            console.log('here');
+            decimal.addEventListener('click', numInput);
+            decFlag = 0;
+        }
     }
-    let value = singleNum(currentArray);
+    else {
+        if (display.textContent[display.textContent.length-1] === '.') {
+            decimal.addEventListener('click', numInput); 
+            decFlag = 0;   
+        }
+        display.textContent = display.textContent.slice(0,-1);
+    }
 }
 
-function numberCall(currentArray) {
-    return currentArray.match(/[+*-\/]/g);
-}
+
